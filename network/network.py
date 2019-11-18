d2116992a1d8117e2aa85c7c8644f80c9bc9e2d9
user.name <cpaik1004@gmail.com>
2019-11-17 22:00:37 -0500

    put L2Normalize wrapper back in

--- network/network.py
+++ network/network.py
@@ -4,36 +4,50 @@ from tensorflow.keras.layers import BatchNormalization, Dense, Dropout, Input, A
     GlobalAveragePooling2D
 
 from base.base_model import BaseModel
-from utils.layers import L2Normalize, Arccos
+from utils.layers import L2Normalize
+from utils.losses import _arcface
+from functools import partial
+from tensorflow.keras.optimizers import Adam
 
 
 class Res50(BaseModel):
     # Backbone is used for valid and prediction and only outptus 512-sized feature embedding
-    def define_backbone(self,model_name: str):
-        input = Input(shape=(self.config.data.img_size, self.config.data.img_size, self.config.data.img_channels),
+    def define_backbone(self, model_name: str):
+        image = Input(shape=(self.config.data.img_size, self.config.data.img_size, self.config.data.img_channels),
                       name="img_in")  # check and delete if this is not neccessary
         res50 = tf.keras.applications.ResNet50(include_top=False, input_shape=(None, None, 3))
-        x = res50(input)
+        x = res50(image)
         x = GlobalAveragePooling2D()(x)
         x = BatchNormalization()(x)
         # x = BatchNormalization()(res50.output)
         x = Dropout(0.3)(x)
         x = Dense(512)(x)
         feature = BatchNormalization()(x)
-        backbone = Model(inputs=input, outputs=feature, name=model_name)
+        feature = Lambda(lambda _x: tf.math.l2_normalize(_x))(feature)
+        backbone = Model(inputs=image, outputs=feature, name=model_name)
         return backbone
 
     def build_backbone(self, model_name):
-        return
+        # no explicit training of just backbone, so no need to compile the model. Hence, no implementation is needed
+        pass
 
     # full model is the one with softmax at the end for meta learning stage. It is made with the backbone
-    def define_full(self, backbone,model_name):
-        x = Lambda(lambda _x: tf.math.l2_normalize(_x))(backbone.output)
-        x = L2Normalize(Dense(self.config.data.n_classes))(x)
-        x = Arccos(self.config.model.hyperparameters.m, self.config.model.hyperparameters.s)(x)
-        x = Activation("softmax")(x)
-        full_model = Model(inputs=backbone.inputs, outputs=x, name=model_name)
+    def define_full(self, backbone, model_name):
+        image = Input(shape=(self.config.data.img_size, self.config.data.img_size, self.config.data.img_channels),
+                      name="img_in_full")  # check and delete if this is not neccessary
+        feature = backbone(image)
+        # logit_pre = Dense(self.config.data.n_classes)(feature)
+        logit_pre = L2Normalize(Dense(units=self.config.data.n_classes))(feature)  # dim: n_class
+        full_model = Model(inputs=image, outputs=logit_pre, name=model_name)
         return full_model
 
-    def build_full(self, model_name):
-        return
+    def build_full(self, backbone, model_name):
+        # create arcface loss that only takes in y_pred and y_true
+        arcface = partial(_arcface, m=self.config.model.hyperparameters.m, s=self.config.model.hyperparameters.s)
+        arcface.__name__ = "arcface_loss"
+        full_model = self.define_full(backbone=backbone, model_name=model_name)
+        opt = Adam(lr=self.config.model.hyperparameters.lr, beta_1=self.config.model.hyperparameters.beta1,
+                   beta_2=self.config.model.hyperparameters.beta2,
+                   clipvalue=self.config.model.hyperparameters.clipvalue, clipnorm=self.config.model.hyperparameters.clipnorm)
+        full_model.compile(optimizer=opt, loss=arcface, metrics=["accuracy"])
+        return full_model
