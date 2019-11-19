1fb3f4cdd5c2abb8ed6ceb1673c6912562da138c
user.name <cpaik1004@gmail.com>
2019-11-18 20:17:18 -0500

    Added code for quanitization using tflite

--- quantize.py
+++ quantize.py
@@ -1,10 +1,18 @@
 import tensorflow as tf
 import argparse
 import os
+from utils.process_config import process_config
+from utils.layers import L2Normalize
 
 
 def main(config_file, model_name):
-
+    config = process_config(config_file)
+    model_file = os.path.join(config.exp.saved_model_dir, model_name)
+    custom_objects = {"tf": tf, "L2Normalize": L2Normalize}
+    model = tf.keras.models.load_model(filepath=model_file, custom_objects=custom_objects, compile=False)
+    converter = tf.lite.TFLiteConverter.from_keras_model(model)
+    converter.optimizations = [tf.lite.Optimize.DEFAULT]
+    tflite_model = converter.convert()
     return
 
 
@@ -14,4 +22,3 @@ if __name__ == "__main__":
     ap.add_argument("--model_name", type=str, default="model.hdf5", help="config path to use")
     args = vars(ap.parse_args())
     main(config_file=args["config"], model_name=args["model_name"])
-    main()
