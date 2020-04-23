WIP FOR FIXING THE NORMALIZATION LAYER!!

dataloader/dataloader.py
@@ -28,19 +28,24 @@ def _generator(valid_path, img_size):
 
 
 class Dataloader(BaseDataLoader):
-    def __init__(self, config):
+    def __init__(self, config, init_train=True):
         super().__init__(config)
         self.train_ds = None
         self.valid_ds = None  # valid dataset for true negative pairs
-        self.train_size = len(glob(self.config.data.train_faces_path))
+        self.test_ds = None
+
         self.valid_size = len(glob(self.config.data.valid_faces_path))
+        self.test_size = len(glob(self.config.data.test_faces_path))
 
         # initialize datasets
-        self.init_train_ds()
+        if init_train:
+            self.train_size = len(glob(self.config.data.train_faces_path))
+            self.init_train_ds()
         self.init_valid_ds()
+        self.init_test_ds()
 
     @staticmethod
-    def _process_path_train(file_path, depth, on_value=1.0, off_value=0.0):
+    def _process_path(file_path, depth, on_value=1.0, off_value=0.0):
         label_int = int(tf.strings.split(file_path, "/")[-2])
         label = tf.one_hot(indices=label_int, depth=depth, on_value=on_value, off_value=off_value)
         img = tf.io.read_file(file_path)
@@ -49,13 +54,32 @@ class Dataloader(BaseDataLoader):
         return img, label
 
     def init_train_ds(self):
-        process_path_train = partial(self._process_path_train, depth=self.config.data.n_classes, on_value=1.0,
+        process_path_train = partial(self._process_path, depth=self.config.data.n_classes, on_value=1.0,
                                      off_value=0.0)
         self.train_ds = tf.data.Dataset.list_files(self.config.data.train_faces_path)
         self.train_ds = self.train_ds.shuffle(self.train_size)
         self.train_ds = self.train_ds.map(process_path_train, num_parallel_calls=self.config.data.n_workers)
         self.train_ds = self.train_ds.batch(batch_size=self.config.trainer.train_batch_size)
         self.train_ds = self.train_ds.prefetch(1)
+        return
+
+    @staticmethod
+    def _process_path_test(file_path, depth, on_value=1.0, off_value=0.0):
+        label_int = int(tf.strings.split(file_path, "/")[-2])
+        img = tf.io.read_file(file_path)
+        img = tf.image.decode_jpeg(img, channels=3)
+        img = tf.image.resize(img, [112, 112])
+        img = tf.image.convert_image_dtype(img, tf.float32)
+        return img, label_int
+
+    def init_test_ds(self):
+        process_path = partial(self._process_path_test, depth=self.config.data.n_classes, on_value=1.0,
+                               off_value=0.0)
+        self.test_ds = tf.data.Dataset.list_files(self.config.data.test_faces_path)
+        self.test_ds = self.test_ds.shuffle(self.test_size)
+        self.test_ds = self.test_ds.map(process_path, num_parallel_calls=self.config.data.n_workers)
+        self.test_ds = self.test_ds.batch(batch_size=8)
+        self.test_ds = self.test_ds.prefetch(1)
         return
 
     def init_valid_ds(self):
@@ -75,8 +99,14 @@ class Dataloader(BaseDataLoader):
     def get_valid_data_generator(self):
         return self.valid_ds
 
+    def get_test_data_generator(self):
+        return self.test_ds
+
     def get_train_data_size(self):
         return self.train_size
 
     def get_valid_data_size(self):
         return self.valid_size
+
+    def get_test_data_size(self):
+        return self.test_size

utils/layers.py
@@ -12,20 +12,63 @@ class L2Normalize(Wrapper):
         super().build(input_shape)
         self.layer_depth = int(self.layer.kernel.shape[-1])
         self.kernel_norm_axes = list(range(self.layer.kernel.shape.rank - 1))
+
+    def call(self, inputs, **kwargs):
+        self._update_kernel() # THIS IS THE PROBLEM! WHEN THIS IS COMMENTED IT WORKS!!!
+        output = self.layer(inputs)
+        print(f"output:{output.shape}", tf.norm(output[0]), tf.norm(output[10]))
+        return output
+        # # self._update_kernel()
+        # print("asdf", tf.norm(inputs[0]), inputs[0].shape, tf.norm(inputs[10]), inputs[10].shape)
+        # with tf.name_scope('compute_weights'):
+        #     # Replace kernel by normalized weight variable.
+        #     # self.layer.kernel = tf.nn.l2_normalize(
+        #     #     self.v, axis=self.kernel_norm_axes)
+        #     self.layer.kernel = tf.nn.l2_normalize(self.v)
+        #     print("input", tf.norm(inputs))
+        #     # Ensure we calculate result after updating kernel.
+        #     update_kernel = tf.identity(self.layer.kernel)
+        #     # with tf.control_dependencies([update_kernel]):
+        #     outputs = self.layer(inputs)
+        #     print("output", tf.norm(outputs[0]), tf.norm(outputs[10]))
+        #     return outputs
+
+    def _update_kernel(self):
+        with tf.name_scope('compute_weights'):
+            v = tf.identity(self.layer.kernel)
+            self.layer.kernel = tf.nn.l2_normalize(v, -1)
+
+    def compute_output_shape(self, input_shape):
+        return tf.TensorShape(
+            self.layer.compute_output_shape(input_shape).as_list())
+
+
+class _L2Normalize(Wrapper):
+    def __init__(self, layer, **kwargs):
+        super().__init__(layer, **kwargs)
+        self.layer = layer
+        # self.name = self.layer.name + "_l2normed"
+
+    def build(self, input_shape=None):
+        super().build(input_shape)
+        self.layer_depth = int(self.layer.kernel.shape[-1])
+        self.kernel_norm_axes = list(range(self.layer.kernel.shape.rank - 1))
         self.v = self.layer.kernel
 
     def call(self, inputs, **kwargs):
         # self._update_kernel()
+        # print("asdf", tf.norm(inputs[0]), inputs[0].shape, tf.norm(inputs[10]), inputs[10].shape)
         with tf.name_scope('compute_weights'):
             # Replace kernel by normalized weight variable.
-            self.layer.kernel = tf.nn.l2_normalize(
-                self.v, axis=self.kernel_norm_axes)
-
+            # self.layer.kernel = tf.nn.l2_normalize(
+            #     self.v, axis=self.kernel_norm_axes)
+            self.layer.kernel = tf.nn.l2_normalize(self.v)
             # Ensure we calculate result after updating kernel.
             update_kernel = tf.identity(self.layer.kernel)
-            with tf.control_dependencies([update_kernel]):
-                outputs = self.layer(inputs)
-                return outputs
+            # with tf.control_dependencies([update_kernel]):
+            outputs = self.layer(inputs)
+            print(f"output{outputs.shape}", tf.norm(outputs[0]), tf.norm(outputs[10]))
+            return outputs
 
     def compute_output_shape(self, input_shape):
         return tf.TensorShape(

utils/valid_helper.py
@@ -1,5 +1,4 @@
 import tensorflow as tf
-import numpy as np
 
 
 def cos_similarity(src_feature, tar_feature):
@@ -8,4 +7,5 @@ def cos_similarity(src_feature, tar_feature):
 
 def face_match(src_feature, tar_feature, threshold=0.3):
     sim = cos_similarity(src_feature=src_feature, tar_feature=tar_feature)
-    return sim <= threshold # for cosine similarity, smaller is better
+    result = tf.abs((1 - sim)) <= threshold
+    return result  # for cosine similarity, smaller is better